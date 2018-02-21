f016c68a6576b2026dedd9c9d08ad99611364729
Gabriel Silveira <gabriel.silveira@poli.ufrj.br>
2018-02-21 20:59:16 -0300

    Added table customization and more methods to User class

--- classes/user.php
+++ classes/user.php
@@ -6,78 +6,238 @@
 class User
 {
 	/**
-	 * Stores the connection with the database
-	 * @var PDO
-	 */
-    private $db;
+ 	 * Stores the connection with the database
+ 	 * @var PDO
+ 	 */
+	private $connection;
 
-    private $user_id;
+	private $dbTables = [
+		  "userTable" => "users",
+		  "accessTable" => "access_levels"
+	  ];
 
-    /**
-     * Receives a PDO connection as the first argument. The constructor can also
-     * take an user id as an optional parameter. In this case the class will
-	  * properly handle the user id and update the passed variable making
-     * possible to use PHP sessions to store the user's id.
-	  *
-	  * @param PDO $connection A PDO object connected to the users database
-	  * @param int $user_id Optional. If a variable is passed here it will be
-	  * modified as needed, such as during login.
-     */
-    public function __construct($connection, &$user_id = null)
-    {
-        $this->db = $connection;
-        $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        $this->user_id = &$user_id;
-    }
+	private $userTable = [
+		  "id" => "user_id",
+		  "email" => "email",
+		  "name" => "name",
+		  "password" => "password",
+		  "accessLevel" => "access_level"
+	  ];
 
-    public function register($email, $password, $name)
-    {
-        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+	private $accessTable = [
+			"id" => "level_id",
+			"level" => "level"
+		];
 
-        $query = $this->db->prepare("INSERT INTO users (email, name, password) VALUES (?, ?, ?)");
-        $query->execute([$email, $name, $passwordHash]);
-    }
+	private $id;
 
-    public function login($email, $password)
-    {
-        $query = $this->db->prepare("SELECT user_id, password FROM users WHERE email=?");
-        $query->execute([$email]);
+	private $name;
 
-        if (!$query->rowCount()) {
-            return false;
-        }
+	private $email;
 
-        $userData = $query->fetch();
+	private $accessLevel;
 
-        if (password_verify($password, $userData["password"])) {
-            $this->user_id = $userData["user_id"];
-            return true;
-        }
-        return false;
-    }
+	/**
+	 * Receives a PDO connection as the first argument. The constructor can also
+ 	 * take an user id as an optional parameter. In this case the class will
+ 	 * properly handle the user id and update the passed variable making
+ 	 * possible to use PHP sessions to store the user's id.
+ 	 *
+ 	 * @param PDO $connection A PDO object connected to the users database
+ 	 * @param int $userId Optional. If a variable is passed here it will be
+ 	 * modified as needed, such as during login.
+ 	 */
+	public function __construct($connection, &$userId = null, $dbTables = [], $userTable = [], $accessTable = [])
+	{
+		$this->connection = $connection;
+		$this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+		$this->id = &$userId;
 
-    public function isLogged()
-    {
-        return $this->user_id ? true : false;
-    }
+		foreach ($dbTables as $column => $columnName) {
+			if (isset($this->dbTables[$column]))
+				$this->dbTables[$column] = $columnName;
+		}
 
-    public function getFullName()
-    {
-        $query = $this->db->prepare("SELECT name FROM users WHERE user_id=?");
-        $query->execute([$this->user_id]);
-        return $query->fetch()["name"];
-    }
+		foreach ($userTable as $column => $columnName) {
+			if (isset($this->userTable[$column]))
+				$this->userTable[$column] = $columnName;
+		}
 
-    public function getFirstName()
-    {
-        $fullName = $this->getFullName();
-        $firstName = strstr($this->getFullName(), " ", true);
-        return $firstName ? $firstName : $fullName;
-    }
+		if ($accessTable === null) {
+			unset($this->accessTable);
+			unset($this->userTable["accessLevel"]);
+		} else {
+			foreach ($accessTable as $column => $columnName) {
+				if (isset($this->accessTable[$column]))
+				$this->accessTable[$column] = $columnName;
+			}
+		}
+	}
 
-    public function logout()
-    {
-        $this->user_id = null;
-        session_destroy();
-    }
+	private function retrieveData()
+	{
+		$query = $this->connection->prepare( (isset($this->accessTable) ?
+
+			"SELECT {$this->userTable["email"]}, {$this->userTable["name"]}, " .
+			"{$this->userTable["password"]}, {$this->accessTable["level"]} ".
+			"FROM {$this->dbTables['userTable']} INNER JOIN " .
+			"{$this->dbTables["accessTable"]} ON " .
+			"{$this->dbTables["accessTable"]}.{$this->accessTable["id"]} = " .
+			"{$this->dbTables["userTable"]}.{$this->userTable["accessLevel"]} " .
+			"WHERE {$this->userTable["id"]}=?"
+			:
+			"SELECT {$this->userTable["email"]}, {$this->userTable["name"]}, " .
+			"{$this->userTable["password"]} FROM {$this->dbTables['userTable']} " .
+			"WHERE {$this->userTable["id"]}=?"
+		));
+		$query->execute([$this->id]);
+		$result = $query->fetch();
+
+		$this->email = $result[$this->userTable["email"]];
+		$this->name = $result[$this->userTable["name"]];
+		$this->accessLevel = $result[$this->accessTable["level"]];
+	}
+
+	public function register($email, $password, $name)
+	{
+		if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+			throw new Exception("Invalid Email");
+			
+		$query = $this->connection->prepare(
+			"SELECT {$this->userTable["id"]} FROM {$this->dbTables['userTable']} ".
+			"WHERE {$this->userTable["email"]}=?"
+		);
+
+		$query->execute([$email]);
+
+		if ($query->rowCount()) {
+			throw new Exception("The email already exists in database");
+		}
+
+		$password = password_hash($password, PASSWORD_DEFAULT);
+
+		$query = $this->connection->prepare(
+			"INSERT INTO {$this->dbTables['userTable']} " .
+			"({$this->userTable["email"]}, {$this->userTable["name"]}, " .
+			"{$this->userTable["password"]}) VALUES (?, ?, ?)"
+		);
+
+		$query->execute([$email, $name, $password]);
+	}
+
+	public function login($email, $password)
+	{
+		$query = $this->connection->prepare(
+			"SELECT {$this->userTable["id"]}, {$this->userTable["password"]} " .
+			"FROM {$this->dbTables['userTable']} " .
+			"WHERE {$this->userTable["email"]}=?"
+		);
+
+		$query->execute([$email]);
+
+		if (!$query->rowCount())
+			throw new Exception("User not found in the database");
+
+		$userData = $query->fetch();
+
+		if (!password_verify($password, $userData["password"]))
+			throw new Exception("Incorrect password");
+
+		$this->id = $userData["user_id"];
+	}
+
+	public function isLogged()
+	{
+		return $this->id ? true : false;
+	}
+
+	public function getName()
+	{
+		if (!isset($this->name))
+			$this->retrieveData();
+		return $this->name;
+	}
+
+	public function getEmail()
+	{
+		if (!isset($this->email))
+			$this->retrieveData();
+		return $this->email;
+	}
+
+	public function getAccessLevel()
+	{
+		if (!isset($this->accessTable))
+			return false;
+		if (!isset($this->accessLevel))
+			$this->retrieveData();
+		return $this->accessLevel;
+	}
+
+	public function modifyName($name)
+	{
+		$query = $this->connection->prepare(
+			"UPDATE {$this->dbTables['userTable']} SET " .
+			"{$this->userTable["name"]}=? WHERE {$this->userTable["id"]}=?"
+		);
+
+		$query->execute([$name, $this->id]);
+	}
+
+	public function modifyEmail($email)
+	{
+		if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+			throw new Exception("Invalid Email");
+
+		$query = $this->connection->prepare(
+			"UPDATE {$this->dbTables['userTable']} SET " .
+			"{$this->userTable["email"]}=? WHERE {$this->userTable["id"]}=?"
+		);
+
+		$query->execute([$email, $this->id]);
+	}
+
+	public function modifyPassword($password)
+	{
+		$password = password_hash($password, PASSWORD_DEFAULT);
+
+		$query = $this->connection->prepare(
+			"UPDATE {$this->dbTables['userTable']} SET " .
+			"{$this->userTable["password"]}=? WHERE {$this->userTable["id"]}=?"
+		);
+
+		$query->execute([$password, $this->id]);
+	}
+
+	public function modifyAccessLevel($accessLevel)
+	{
+		$query = $this->connection->prepare(
+			"SELECT {$this->accessTable["id"]} FROM ".
+			"{$this->dbTables['accessTable']} WHERE {$this->accessTable["level"]}=?"
+		);
+
+		$query->execute([$accessLevel]);
+
+		if (!$query->rowCount()) {
+			throw new Exception("Invalid access level");
+		}
+
+		$accessLevel = $query->fetch()[$this->accessTable["id"]];
+
+		$query = $this->connection->prepare(
+			"UPDATE {$this->dbTables['userTable']} SET " .
+			"{$this->userTable["accessLevel"]}=? WHERE {$this->userTable["id"]}=?"
+		);
+
+		$query->execute([$accessLevel, $this->id]);
+	}
+
+	public function logout()
+	{
+		$this->id = null;
+		$this->name = null;
+		$this->email = null;
+		$this->accessLevel = null;
+		session_destroy();
+	}
 }
