2666a8a158d262d2cab11830b7a8b698c1c0ed61
Gabriel Silveira <gabriel.silveira@poli.ufrj.br>
2019-01-13 23:22:30 -0200

    Specify utf8 encoding in the database driver

--- src/setup.php
+++ src/setup.php
@@ -1,16 +1,18 @@
 <?php
 require '../local/config.php';
 
-function autoloader($class)
-{
-    include_once '../local/classes/' . $class . '.php';
+
+function autoloader($class) {
+	include_once '../local/classes/' . $class . '.php';
 }
+
 
 spl_autoload_register("autoloader");
 
 session_start();
 
 
-$connection = new PDO("mysql:host=$host;dbname=$dbname", $dbuser, $password);
+$connection =
+	new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $dbuser, $password);
 $userDataAccessor = new UserDataAccessor($connection);
 $user = User::restoreSession($userDataAccessor, $_SESSION["user_id"]);
