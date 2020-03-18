5f1d20e685f549a28f9d440366b8c17882e14b8d
Gabriel Silveira <gabriel.silveira@poli.ufrj.br>
2020-03-18 18:07:59 -0300

    Update guest page to display contents of custom HTML file

--- partials/guest-dashboard.php
+++ partials/guest-dashboard.php
@@ -1,8 +1,9 @@
 <section class="container section">
 	<div class="columns is-multiline is-centered">
 		<div class="column is-three-fifths">
-			<h1 class="title">Database Index</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<div class="section">
+				<?php echo file_get_contents('../config/guest-message.html'); ?>
+			</div>
 		</div>
 		<div class="column is-three-fifths-tablet is-two-fifths-desktop">
 			<div class="card-header">
