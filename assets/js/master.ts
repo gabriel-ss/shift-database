96c17c7dd6aeb4bd80b7beac89b0f23bf3b29212
Gabriel Silveira <gabriel.silveira@poli.ufrj.br>
2020-03-18 18:14:00 -0300

    Prepare pages to render preact components

--- assets/js/master.ts
+++ assets/js/master.ts
@@ -1,3 +1,18 @@
-import UserUI from "./components/user/UI";
-import AdminUI from "./components/admin/UI.js";
+if (process.env.NODE_ENV === "development") require("preact/debug");
+import {render, h} from "preact";
+import UserUI from "./components/user/App";
+import AdminUI from "./components/admin/App";
 import "bulma/bulma.sass";
+
+declare global {
+	interface Window {
+		config: any;
+	}
+}
+
+const app = document.querySelector("#app")!;
+
+(document.querySelector("#master-script") as HTMLScriptElement)
+	.dataset.userType === "admin"
+	? render(h(AdminUI, {}), app)
+	: render(h(UserUI, {}), app);
