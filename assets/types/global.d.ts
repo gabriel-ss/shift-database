ddf36e277a6fad004ef0de46ac20ef856b324abd
Gabriel Silveira <gabriel.silveira@poli.ufrj.br>
2020-03-23 22:45:28 -0300

    Apply correct type to config object

--- assets/types/global.d.ts
+++ assets/types/global.d.ts
@@ -1,3 +1,8 @@
 interface Window {
-	config: any;
+	config: {
+		DEFAULT_SHIFT_CAPACITY: number;
+		DEFAULT_SCHEDULE: Record<string, number[]>;
+		SUBSCRIPTION_TIME_LIMIT: number;
+		UNSUBSCRIPTION_TIME_LIMIT: number;
+	};
 }
